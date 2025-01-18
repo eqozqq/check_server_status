b3fc840d041891606f45e07c6cb19237d403b752
eqozqq <112166693+eqozqq@users.noreply.github.com>
2025-01-18 21:07:43 +0200

    Update check_server_status.php

--- check_server_status.php
+++ check_server_status.php
@@ -1,4 +1,12 @@
 <?php
+header("Access-Control-Allow-Origin: https://www.nostalgiaforum.xyz");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 $ip = $_GET['ip'];
 $port = $_GET['port'];
 $fp = fsockopen("udp://$ip", $port, $errno, $errstr, 1);
