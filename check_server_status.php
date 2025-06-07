313b1434324c8c65eeb29b7b23e967f996af6e01
eqozqq <112166693+eqozqq@users.noreply.github.com>
2025-06-07 10:48:38 +0300

    Update check_server_status.php

--- check_server_status.php
+++ check_server_status.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: https://legacyminecraftpe.github.io/");
+header("Access-Control-Allow-Origin: https://legacyminecraftpe.github.io");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
